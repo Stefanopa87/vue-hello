aeb517337e4d59fbce67e41d36d34a03e02fcb66
Stefano Patti <mastepa@hotmail.com>
2021-04-06 16:03:21 +0200

    es done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// GOAL: stampare a schermo il contenuto
+// di una variabile dentro un h1
+//
+// BONUS: utilizzare un data come nome
+// del file immagine in un tag img
+
+
+function init(){
+
+  new Vue({
+
+      el: '#app',
+      data: {
+        'msg': 'Questo testo è scritto con Vue JS..',
+      }
+  });
+}
+
+$(document).ready(init);
